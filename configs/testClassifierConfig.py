2553dd8af0e48217bb477940450dbdec4be6fdcd
ManuelSzewc <56162512+ManuelSzewc@users.noreply.github.com>
2019-10-04 13:55:04 +0000

    Update testClassifierConfig.py
    Add MultiCore option and offset to gensim parameters

--- configs/testClassifierConfig.py
+++ configs/testClassifierConfig.py
@@ -7,8 +7,12 @@ sb=0.1
 # number of signal events
 Ns=round(sb*Nb)
 
-# gensim parameters (keep the format as a string here)
-gensim_parameters="num_topics=2,alpha=[0.9,0.1],passes=20,iterations=100,gamma_threshold=0.00000001,eval_every=10,update_every=1,chunksize=1,decay=0.5"
+#Set Multicore to 1 for Multicore (less time, more cores involved)
+Multicore=1
+# gensim parameters for Multicore LDA (keep the format as a string here)
+gensim_parameters_Multicore="num_topics=2,alpha=[2.727,0.273],passes=300,iterations=100,gamma_threshold=0.00000001,eval_every=10,workers=2,chunksize=10000,offset=1000.0"
+# gensim parameters for old LDA (keep the format as a string here)
+gensim_parameters="num_topics=2,alpha=[0.9,0.1],passes=20,iterations=100,gamma_threshold=0.00000001,eval_every=10,update_every=1,chunksize=1,decay=0.5, offset=1000.0"
 # the alpha parameters are the hyper-parameters that determine the topic concentrations in the docs and in the corpus
 # the passes is how many times you train over the whole dataset
 # for details on other parameters we refer the reader to the gensim webpage
